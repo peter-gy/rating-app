292b2d91f8324759d0af109bc35d91ed8b81fe69
Péter Ferenc Gyarmati <dev.petergy@gmail.com>
2020-09-30 22:55:42 +0200

    Updates

---
+++ precache-manifest.6297778ae583f71227a85995768bf536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f762fe90e8316f42892416389b143abe",
+    "url": "/rating-app/index.html"
+  },
+  {
+    "revision": "a37ddb943b201be8f78a",
+    "url": "/rating-app/static/css/main.3290d2d4.chunk.css"
+  },
+  {
+    "revision": "46aad1d5d619459f83e2",
+    "url": "/rating-app/static/js/2.8b134225.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rating-app/static/js/2.8b134225.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37ddb943b201be8f78a",
+    "url": "/rating-app/static/js/main.137571de.chunk.js"
+  },
+  {
+    "revision": "33144ff2a694a6e5a84b",
+    "url": "/rating-app/static/js/runtime-main.cba2409d.js"
+  }
+]);
